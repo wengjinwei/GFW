e07a2373b9b615fed9b78943cd1a6af6c92cd4e4
cuso1985 <cuso1985@users.noreply.github.com>
2020-05-03 07:08:50 +0800

    update

--- Surge/Scripts/wifi_proxy.js
+++ Surge/Scripts/wifi_proxy.js
@@ -1,6 +1,6 @@
 /* wifi_proxy change
 [Script]
-wifi_proxy.js = debug=1,script-path=Scripts/wifi_proxy.js,type=event,event-name=network-changed
+wifi_proxy.js = debug=1,script-path=https://raw.githubusercontent.com/rainman0925/GFW/master/Surge/Scripts/wifi_proxy.js,type=event,event-name=network-changed
 
 PS:记得自己修改WIFI名称
 主要功能:指定Wi-Fi(路由器翻)下,Surge使用直连模式,其他网络下Surge使用规则模式
@@ -15,11 +15,11 @@ var wifiname = $network.wifi.ssid;
 var proxywifi = "lao duan_6G";
 if (wifiname == proxywifi){
     $surge.setOutboundMode("direct");
-    $notification.post("rainman_Remind","您目前处于WIFI-Proxy","Surge已自动变为直连模式");
+    $notification.post("SSID已启动","Surge已自动变为直连模式");
     
 }
 else{
     $surge.setOutboundMode("rule");
-    $notification.post("rainman_Remind","Surge已自动变为规则模式","");
+    $notification.post("SSID已退出","Surge已自动变为规则模式");
 }
 $done();
