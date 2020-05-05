bab9e9d05707508b3a0ee32007be373f8e6f891e
cuso1985 <cuso1985@users.noreply.github.com>
2020-05-05 18:10:05 +0800

    update

--- Surge/Scripts/wifi_proxy.js
+++ Surge/Scripts/wifi_proxy.js
@@ -15,11 +15,11 @@ var wifiname = $network.wifi.ssid;
 var proxywifi = "lao duan_6G";
 if (wifiname == proxywifi){
     $surge.setOutboundMode("direct");
-    $notification.post("SSID已启动","Surge已自动变为直连模式");
+    $notification.post("SSID已启动","Surge已自动变为直连模式","");
     
 }
 else{
     $surge.setOutboundMode("rule");
-    $notification.post("SSID已退出","Surge已自动变为规则模式");
+    $notification.post("SSID已退出","Surge已自动变为规则模式","");
 }
 $done();
